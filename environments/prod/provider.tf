07386d241ccb34270aeabcb52366b8f652344709
A K Verma <amitkkc01@gmail.com>
2025-11-09 00:09:28 +0530

    Update Azure backend configuration for production

--- environments/prod/provider.tf
+++ environments/prod/provider.tf
@@ -6,14 +6,14 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name  = "rg-devopsinsiders"
-    storage_account_name = "twostates"
-    container_name       = "tfstate"
-    key                  = "prod.tfstate"
+    resource_group_name  = "tfstatefile_rg_for_all_do_not_delete"
+    storage_account_name = "tfstatergb17g2devops"
+    container_name       = "tfstatefilecontainer"
+    key                  = "prodtodo.tfstate"
   }
 }
 
 provider "azurerm" {
   features {}
-  subscription_id = "1075ec7a-b17a-4f37-bf3f-9d68c4506dc1"
+  subscription_id = "c0748677-9808-4356-8816-dc8088c5bb59"
 }
